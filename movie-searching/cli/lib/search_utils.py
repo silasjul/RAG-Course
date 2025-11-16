12c5c33bea615aa353378d30654434ec54aaf73d
silasjul <silaskierstein@gmail.com>
2025-11-16 12:05:13 +0100

    semantic search and chunking

--- movie-searching/cli/lib/search_utils.py
+++ movie-searching/cli/lib/search_utils.py
@@ -10,6 +10,8 @@ BM25_K1 = 1.5
 # Document length normalization strength (so that longer description arent favored over shorter once as they might be just as relevant)
 BM25_B = 0.75
 
+SCORE_PRECISION = 4
+
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
 STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
